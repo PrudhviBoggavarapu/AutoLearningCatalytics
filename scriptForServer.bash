04cfa743e14ad4998c0766db72584ad6aba4305f
Karma Jade Rose <codingpabs@gmail.com>
2023-09-15 03:20:31 -0500

    Fixing Script, deleating service

--- scriptForServer.bash
+++ scriptForServer.bash
@@ -2,7 +2,12 @@
 nohup python3 main.py 
 while true
 do
-! git pull --force | grep -q "Already up to date" && killall firefox && nohup python3 main.py 
+! git pull --force | grep -q "Already up to date" && killall firefox 
 sleep 10
 echo "DoneLoop"
+done&
+
+while true 
+do 
+python3 main.py
 done
