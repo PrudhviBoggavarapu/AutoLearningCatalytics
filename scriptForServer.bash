9a7320825ddb42a1315a0ed51b5abd3ac2f15be9
Karma Jade Rose <codingpabs@gmail.com>
2023-09-15 03:07:14 -0500

    Fixed Script

--- scriptForServer.bash
+++ scriptForServer.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-! git pull --force | grep -q "Already up to date" && killall firefox && python3 main.py
+! git pull --force | grep -q "Already up to date" && killall firefox && nohup python3 main.py 
 sleep 10
 echo "DoneLoop"
 done
